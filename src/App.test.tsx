1b5aeb198c6ff9c0800900a4c7fc1093942e13c4
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-09-03 13:38:27 +0200

    ncu -u

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Home link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText("Home");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Movies link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText("Movies");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Users link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText("Users");
   expect(linkElement).toBeInTheDocument();
 });
