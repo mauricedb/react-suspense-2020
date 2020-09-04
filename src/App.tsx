5f772bd4df404d88cd26df9eee4b10ac80301159
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-09-04 16:41:28 +0200

    Using React.lazy() (not working yet)

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
-import Movies from "./components/Movies";
-import Users from "./components/Users";
-import UserDetails from "./components/UserDetails";
+// import Movies from "./components/Movies";
+// import Users from "./components/Users";
+// import UserDetails from "./components/UserDetails";
+
+const Movies = React.lazy(() => import("./components/Movies"));
+const Users = React.lazy(() => import("./components/Users"));
+const UserDetails = React.lazy(() => import("./components/UserDetails"));
 
 const App: React.FC = () => (
   <BrowserRouter>
