2c0ef9334e7e96b10f4237d47b5f15c56dd49827
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-09-08 12:04:12 +0200

    Using SuspenseList

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -4,14 +4,14 @@ import UserMovieDetails from "./UserMovieDetails";
 import Loading from "./Loading";
 
 const UserDetails: React.FC = () => (
-  <>
+  <React.unstable_SuspenseList revealOrder="forwards" tail="collapsed">
     <Suspense fallback={<Loading />}>
       <UserUserDetails />
     </Suspense>
     <Suspense fallback={<Loading />}>
       <UserMovieDetails />
     </Suspense>
-  </>
+  </React.unstable_SuspenseList>
 );
 
 export default UserDetails;
